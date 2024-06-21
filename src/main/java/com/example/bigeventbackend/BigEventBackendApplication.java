5ee2004b8ba3aad17d41c7ef918aff1b9983f2dd
wanglei <1746465339@qq.com>
2024-06-22 00:17:59 +0800

    Init commit

---
+++ src/main/java/com/example/bigeventbackend/BigEventBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigeventbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigEventBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigEventBackendApplication.class, args);
+	}
+
+}
